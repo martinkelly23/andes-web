620870ecf0f64489466768245d774d542194881b
martinkelly23 <martkelly92@gmail.com>
2018-05-13 16:15:17 -0300

    Feat: Twig was added with template, js and css.
    The view of the site was started, using twig

--- src/Controller/ViewsController.php
+++ src/Controller/ViewsController.php
@@ -2,13 +2,12 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ViewsController
+class ViewsController extends Controller
 {
     public function home()
     {
-        return new Response(
-            '<html><body>Hello Andes Global</body></html>'
-        );
+      return $this->render('index.html.twig');
     }
 }
